05f18373c8d3953d12f7b9bc8c9105a0adee7a0a
Sultan Md. Alnur <98782302+Alnur17@users.noreply.github.com>
2024-12-31 18:08:10 +0600

    some file added

---
+++ lib/app/modules/home/views/widgets/home_widgets/stories_section.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:tails_date/app/data/dummy_data.dart';
+
+import '../story_widgets/add_story_avatar.dart';
+import '../story_widgets/story_avatar.dart';
+
+class StoriesSection extends StatelessWidget {
+  const StoriesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16),
+        child: Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(right: 8.0), // Spacing for "Your Story"
+              child: AddStoryAvatar(),
+            ),
+            ...List.generate(DummyData.stories.length, (index) {
+              final story = DummyData.stories[index];
+              return Padding(
+                padding: EdgeInsets.only(
+                  right: index == DummyData.stories.length - 1 ? 0 : 8.0, // No padding for the last item
+                ),
+                child: StoryAvatar(story: story),
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
