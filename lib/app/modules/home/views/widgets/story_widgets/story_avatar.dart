4e93ea282e0e9832cd6b6549d784ebb08ebf619c
Sultan Md. Alnur <98782302+Alnur17@users.noreply.github.com>
2025-09-07 13:03:35 +0600

    clean some code

--- lib/app/modules/home/views/widgets/story_widgets/story_avatar.dart
+++ lib/app/modules/home/views/widgets/story_widgets/story_avatar.dart
@@ -1,49 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-//
-// import '../../../../../../common/app_color/app_colors.dart';
-// import '../../../../../../common/app_text_style/styles.dart';
-// import '../../../../../../common/size_box/custom_sizebox.dart';
-// import '../../story_view.dart';
-//
-// class StoryAvatar extends StatelessWidget {
-//   final Map<String, String> story;
-//
-//   const StoryAvatar({required this.story, super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         GestureDetector(
-//           onTap: () {
-//             Get.to(() => const StoryView());
-//           },
-//           child: CircleAvatar(
-//             radius: Get.width * 0.094,
-//             backgroundColor: AppColors.black,
-//             child: CircleAvatar(
-//               radius: Get.width * 0.09,
-//               backgroundColor: AppColors.mainColor,
-//               child: CircleAvatar(
-//                 radius: Get.width * 0.085,
-//                 backgroundImage: NetworkImage(story['imageUrl']!),
-//               ),
-//             ),
-//           ),
-//         ),
-//         sh8,
-//         Text(
-//           story['name']!,
-//           style: h7.copyWith(fontWeight: FontWeight.w700),
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -53,7 +7,6 @@ import '../../../../../../common/size_box/custom_sizebox.dart';
 import '../../../controllers/story_controller.dart';
 import '../../../model/all_author_story_model.dart';
 import '../../story_view.dart';
-
 
 class StoryAvatar extends StatelessWidget {
   final AllAuthDatum story;
@@ -72,8 +25,12 @@ class StoryAvatar extends StatelessWidget {
             controller.isLoadingStories.value = false;
             //if (controller.storyImageUrls.isNotEmpty) {
             if (controller.authorStories.value?.data.isNotEmpty ?? false) {
-              final firstStoryId = controller.authorStories.value!.data.first.id;
-              Get.to(() => StoryView(authorName: story.name ?? 'Unknown', storyId: firstStoryId!,));
+              final firstStoryId =
+                  controller.authorStories.value!.data.first.id;
+              Get.to(() => StoryView(
+                    authorName: story.name ?? 'Unknown',
+                    storyId: firstStoryId!,
+                  ));
             }
             // else {
             //   kSnackBar(
@@ -116,54 +73,3 @@ class StoryAvatar extends StatelessWidget {
     );
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import '../../../../../../common/app_color/app_colors.dart';
-// import '../../../../../../common/app_text_style/styles.dart';
-// import '../../../../../../common/size_box/custom_sizebox.dart';
-// import '../../../model/all_stories_model.dart';
-// import '../../story_view.dart';
-//
-// class StoryAvatar extends StatelessWidget {
-//   final AllStoryDatum story;
-//
-//   const StoryAvatar({required this.story, super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         GestureDetector(
-//           onTap: () {
-//             Get.to(() => const StoryView());
-//           },
-//           child: CircleAvatar(
-//             radius: Get.width * 0.094,
-//             backgroundColor: AppColors.black,
-//             child: CircleAvatar(
-//               radius: Get.width * 0.09,
-//               backgroundColor: AppColors.mainColor,
-//               child: CircleAvatar(
-//                 radius: Get.width * 0.085,
-//                 backgroundImage: NetworkImage(story.image ?? ''),
-//                 onBackgroundImageError: (error, stackTrace) => Icon(
-//                   Icons.person,
-//                   size: Get.width * 0.06,
-//                   color: AppColors.white,
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//         sh8,
-//         Text(
-//           story.author?.name ?? 'Unknown',
-//           style: h7.copyWith(fontWeight: FontWeight.w700),
-//         ),
-//       ],
-//     );
-//   }
-// }
